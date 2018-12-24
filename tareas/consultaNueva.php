78eefa44449769c0ba2ca49bbe8175f136b28da8
alfredoperaltag <alfredo08peralta@gmail.com>
2018-12-23 23:34:19 -0600

    nueva tarea

--- tareas/consultaNueva.php
+++ tareas/consultaNueva.php
@@ -1,4 +1,24 @@
 <?php
-    include "../conexion.php"
-    
+    $_POST['nombre'];
+    $_POST['descripcion'];    
+    include "../conexion.php";
+    $consulta = "INSERT INTO tareas (nombre,descripcion) VALUES ('$_POST[nombre]','$_POST[descripcion]')";
+    if (empty($_POST['nombre'] && $_POST['descripcion'])) {
+        echo "<script>
+                window.location.href = 'formulario.php';
+                alert('Los campos no pueden estar vacios');
+            </script>";
+    } else {
+        if (mysqli_query($conexion,$consulta)) {
+            echo "<script>
+                    window.location.href = 'tareas.php';                    
+                </script>";
+        } else {
+            echo "<script>
+                    window.location.href = 'formulario.php';
+                </script>";
+            echo "error" . $sql . "<br>" . mysqli_error($conexion);            
+        }        
+    }
+    mysqli_close($conexion);
 ?>
--- tareas/tareas.php
+++ tareas/tareas.php
@@ -23,25 +23,33 @@
     include "../conexion.php";
     $consulta = mysqli_query($conexion, "SELECT * FROM tareas");
     $registro = mysqli_fetch_array($consulta);
-    do{
-        ?>
-            <table class = table>
-                <thead class="thead-dark">
-                    <tr class="text-center">                                           
-                        <th>Nombre</th>
-                        <th>Descripción</th>
-                        <th>Acciones</th>
-                        <th><a href="formulario.php"><button class="btn btn-success">Nueva</button></a></th>                
-                    </tr> 
-                </thead>
-                <tbody class = "text-center">
+
+    ?>
+        <table class = table>
+            <thead class="thead-dark">
+                <tr class="text-center">                                           
+                    <th>Nombre</th>
+                    <th>Descripción</th>
+                    <th>Acciones</th>
+                    <th><a href="formulario.php"><button class="btn btn-success">Nueva</button></a></th>                
+                </tr> 
+            </thead>
+    <?php
+        do{        
+    ?> 
+            <tbody class = "text-center">
+                <tr>
                     <th><?php echo $registro['nombre'] ?></td>
                     <td><?php echo $registro['descripcion'] ?></td>
-                </tbody>                           
-            </table>
-        <?php
-    } while ($registro = mysqli_fetch_array($consulta));
-?>
+                    <td><button class="btn btn-outline-primary">Modificar</button></td>
+                    <td><button class = "btn btn-outline-danger">Eliminar</button></td>
+                </tr>                
+            </tbody>
+    <?php
+        } while ($registro = mysqli_fetch_array($consulta));
+    ?>          
+        </table>
+        
     </div>
 
     <script src="../js/jquery-3.3.1.min.js"></script>
