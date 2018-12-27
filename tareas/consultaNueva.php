fec47102e0c5d1ccb3d7c14aeffe4eaa34542c19
alfredoperaltag <alfredo08peralta@gmail.com>
2018-12-26 19:11:09 -0600

    registrar tarea

--- tareas/consultaNueva.php
+++ tareas/consultaNueva.php
@@ -1,21 +1,21 @@
 <?php
 $_POST['nombre'];
 $_POST['descripcion'];
-include "../conexion.php";
-$consulta = "INSERT INTO tareas (nombre,descripcion) VALUES ('$_POST[nombre]','$_POST[descripcion]')";
 if (empty($_POST['nombre'] && $_POST['descripcion'])) {
     echo "<script>
-                window.location.href = 'formulario.php';
+                window.location.href = 'formularioRegistrar.php';
                 alert('Los campos no pueden estar vacios');
             </script>";
 } else {
+    include "../conexion.php";
+    $consulta = "INSERT INTO tareas (nombre,descripcion) VALUES ('$_POST[nombre]','$_POST[descripcion]')";
     if (mysqli_query($conexion, $consulta)) {
         echo "<script>
-                    window.location.href = 'tareas.php';                    
+                    window.location.href = 'consultaTareas.php';                    
                 </script>";
     } else {
         echo "<script>
-                    window.location.href = 'formulario.php';
+                    window.location.href = 'formularioRegistrar.php';
                 </script>";
         echo "error" . $sql . "<br>" . mysqli_error($conexion);
     }
--- tareas/formularioRegistrar.php
+++ tareas/formularioRegistrar.php
@@ -1,23 +1,22 @@
 <!DOCTYPE html>
 <html>
-<?php
-require "formulario.php";
-$obj = new formulario;
-$obj->headerFormulario();
-?>
-<body>
-<?
-require "barraNavegaciontareas.php";
-?>
-    <div class="container">
-    <form action="consultaNueva.php" method="POST">    
-    <?
-    $nombre = "";
-    $descripcion = "";
-    $obj = new formulario();
-    $obj->cuerpoFormulario($nombre, $descripcion);
+    <?php
+    require "formulario.php";
+    $obj = new formulario;
+    $obj->headerFormulario();
     ?>
-    </form>
+
+<body>
+    <?php
+    require "barraNavegaciontareas.php";
+    ?>
+    <div class="container">
+        <form action="consultaNueva.php" method="POST">    
+            <?php 
+            $obj = new formulario();
+            $obj->cuerpoFormulario("", "");
+            ?>
+        </form>
     </div>
 
     <script src="../js/jquery-3.3.1.min.js"></script>
