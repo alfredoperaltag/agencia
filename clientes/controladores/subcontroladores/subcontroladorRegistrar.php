1e39c82de11738df2954de2d1745b03616d1989d
alfredoperaltag <alfredo08peralta@gmail.com>
2019-01-09 19:32:36 -0600

    pdo registrar

--- clientes/controladores/subcontroladores/subcontroladorRegistrar.php
+++ clientes/controladores/subcontroladores/subcontroladorRegistrar.php
@@ -1,30 +1,34 @@
 <?php
-$_POST['nombre'];
-$_POST['apellidoPaterno'];
-$_POST['apellidoMaterno'];
-$_POST['telefono'];
-$_POST['curp'];
-$_POST['fechanacimiento'];
-$_POST['familiares'];
-$_POST['ruta'];
-$_POST['descripcion'];
+$nombre = $_POST['nombre'];
+$apellidoPaterno = $_POST['apellidoPaterno'];
+$apellidoMaterno = $_POST['apellidoMaterno'];
+$telefono = $_POST['telefono'];
+$curp = $_POST['curp'];
+$fechanacimiento = $_POST['fechanacimiento'];
+$familiares = $_POST['familiares'];
+$ruta = $_POST['ruta'];
+$descripcion = $_POST['descripcion'];
 
 if (empty($_POST["nombre"])) {
-    echo '<script type="text/javascript">
+    ?>
+    <script type="text/javascript">
     window.location.href="../controladorRegistrar.php";
-    alert("¡El campo nombre es obligatorio!");    
-    </script>';
+    alert("¡El campo nombre es obligatorio!");
+    </script>     
+    <?php
+
 } else {
     require "../../modelos/consultaRegistrar.php";
-    if (mysqli_query($conexion, $consulta)) {
-        mysqli_close($conexion);
+    if ($consulta) {
         header('Location: ../controladorClientes.php');
     } else {
-        echo '<script type="text/javascript">
+        ?>
+        <script type="text/javascript">
         alert("¡Ocurrio un problema al registrar!");
         /* window.location.href="../controladorRegistrar.php"; */
         </script>';
-        echo "Error: " . $sql . "<br>" . mysqli_error($conexion);
+        <?php
+        /* echo "Error: " . $sql . "<br>" . mysqli_error($conexion); */
     }
 }
 ?>
--- clientes/modelos/consultaRegistrar.php
+++ clientes/modelos/consultaRegistrar.php
@@ -1,7 +1,20 @@
 <?php
-include "conexion.php";
-$consulta = "INSERT INTO 
-clientes (nombre,apellidoPaterno,apellidoMaterno,telefono,curp,fechanacimiento,familiares,ruta,descripcion) 
-VALUES 
-('$_POST[nombre]','$_POST[apellidoPaterno]','$_POST[apellidoMaterno]','$_POST[telefono]','$_POST[curp]','$_POST[fechanacimiento]','$_POST[familiares]','$_POST[ruta]','$_POST[descripcion]')";
+require "conexion.php";
+$consulta = $conexion->prepare(
+    "INSERT INTO clientes 
+    (nombre,apellidoPaterno,apellidoMaterno,telefono,curp,fechanacimiento,familiares,ruta,descripcion) 
+    VALUES 
+    (:nombre, :apellidoPaterno, :apellidoMaterno, :telefono, :curp, :fechanacimiento, :familiares, :ruta, :descripcion)"
+);
+$consulta->execute(array(
+    ':nombre' => $nombre,
+    ':apellidoPaterno' => $apellidoPaterno,
+    ':apellidoMaterno' => $apellidoMaterno,
+    ':telefono' => $telefono,
+    ':curp' => $curp,
+    ':fechanacimiento' => $fechanacimiento,
+    ':familiares' => $familiares,
+    ':ruta' => $ruta,
+    ':descripcion' => $descripcion
+));
 ?>
