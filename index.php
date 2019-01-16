a2c7e7d5f9d32f3d084e7075d9a10ecfc7aed078
alfredoperaltag <alfredo08peralta@gmail.com>
2019-01-15 20:30:56 -0600

    creacion de controladores

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require_once "controladores/plantilla.controlador.php";
+require_once "controladores/clientes.controlador.php";
+require_once "controladores/tareas.controlador.php";
+
+require_once "modelos/clientes.modelo.php";
+require_once "modelos/tareas.modelo.php";
+
 /* require_once "models/model.php"; */
 $plantilla = new ControladorPlantilla();
 $plantilla->ctrPlantilla();
