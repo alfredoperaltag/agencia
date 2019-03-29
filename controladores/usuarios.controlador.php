351c48c5afa9c41290b6e8416ffc82eae2fc5bd7
alfredoperaltag <alfredo08peralta@gmail.com>
2019-03-29 10:24:09 -0600

    login controlador

--- controladores/usuarios.controlador.php
+++ controladores/usuarios.controlador.php
@@ -9,6 +9,27 @@ class ControladorUsuarios
                 preg_match('/^[a-zA-Z0-9]+$/', $_POST["password"])
             ) {
                 $encriptar = crypt($_POST["password"], '$2a$07$asxx54ahjppf45sd87a5a4dDDGsystemdev$');
+                $tabla = "usuarios";
+                $item = "usuario";
+                $valor = $_POST['nombreUsuario'];
+
+                $respuesta = ModeloUsuarios::MdlMostrarUsuarios($tabla, $item, $valor);
+
+                if ($respuesta['usuario'] == $_POST['nombreUsuario'] && $respuesta['password'] == $encryptar) {
+                    $_SESSION['iniciarSesion'] = "ok";
+                    $_SESSION['id'] = $respuesta['id'];
+                    $_SESSION['nombre'] = $respuesta['nombre'];
+                    $_SESSION['usuario'] = $respuesta['usuario'];
+                    $_SESSION['perfil'] = $respuesta['perfil'];
+                    echo '<script> window.location = "inicio"; </script>';
+                } else {
+                    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+					<strong>¡Usuario o contraseña incorrecta!</strong>
+					<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span class="fa fa-times"></span>
+					</button>
+				</div>';
+                }
             }
         }
     }
--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once "controladores/usuarios.controlador.php";
 require_once "controladores/clientes.controlador.php";
 require_once "controladores/tareas.controlador.php";
 
-
+require_once "modelos/usuario.modelo.php";
 /* require_once "modelos/clientes.modelo.php";
 require_once "modelos/tareas.modelo.php"; */
 
